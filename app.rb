4629087ba8ed58dc2e98c86caf9c9b1d1148ef56
David Yu <david.yu@yale.edu>
2020-04-22 16:32:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 name = "Dave"
 age = 19
 favorite_food = "Spaghetti alla carbonara"
+height_in_inches = (12 * 5) + 11
+height_message = (height > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than averate" : "You are not taller than average"
 
 ## Do not modify below this line!
 
